93019b2b517f43bdc6e4bbdca3aa1343eb02a24d
Konark Sharma <Sharmakonark6k1@gmail.com>
2021-12-06 20:38:58 +0530

    Add files via upload

---
+++ Reverse a Linklist (Iteratively).cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+// Creating Node Class For linklist
+class node{
+    public:
+    int data;
+    node* next; 
+   // Iniatializing a constructor to send val
+    node(int val)
+    {
+        data = val;
+        next =NULL;
+    }
+};
+void insertAtTail(node* &head,int val)
+{
+   node* n = new node(val);
+   node* temp = head;
+   if(head == NULL)
+   {
+       head = n;
+       return;
+   }
+   while(temp->next != NULL)
+   {
+    temp = temp->next;
+   }
+   temp->next = n;
+}
+
+// Taking head by value here
+void display(node* head)
+{
+    node* temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+
+}
+
+node* reverse(node* &head)
+{
+    // Assigning 3 pointer ie. previous,current,next to reverse the linklist
+    node* prev = NULL;
+    node* curr = head;
+    node* next;
+
+    while(curr != NULL)
+    {
+        next =  curr->next;
+        curr->next = prev;
+
+        prev = curr;
+        curr = next;
+    }
+   
+     return prev; 
+}
+int main()
+{
+   node* head = NULL;
+   insertAtTail(head,1);
+   insertAtTail(head,2);
+   insertAtTail(head,3);
+   insertAtTail(head,4);
+   display(head);
+   node* newhead = reverse(head);
+   display(newhead);
+
+   
+    return 0;
+}
